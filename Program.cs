348f6b5590b33c1c759ea55911f14990ebe7e1a9
TrungHauad02 <97101001+TrungHauad02@users.noreply.github.com>
2022-10-01 03:32:50 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,18 +14,26 @@ namespace VoLamTruyenKy
         {
             DanhSachNhanVat NV = new DanhSachNhanVat(5);
             NV.xuatDS();
+            Console.WriteLine("Nhan vat co sat thuong cao nhat: ");
             NV.STCaoNhat().Xuat();
 
             DanhSachQuaiThuong QT = new DanhSachQuaiThuong(6);
             QT.xuatDS();
+            Console.WriteLine("Quai thuong co sat thuong cao nhat: ");
             QT.STCaoNhat().Xuat();
 
             DanhSachQuaiThuLinh QTL = new DanhSachQuaiThuLinh(7);
             QTL.xuatDS();
+            Console.WriteLine("Quai thu linh co sat thuong cao nhat: ");
             QTL.STCaoNhat().Xuat();
 
             double st = NguoiChoi.SatThuong(NV.DSNHANVAT[0], QT.DSQUAITHUONG[2]);
             Console.WriteLine(st);
+            Console.WriteLine("An phim bat ki de tiep tuc");
+            Console.ReadKey();
+            Console.Clear();
+
+           Game game = new Game(NV,QT,QTL);
 
         }
     }
